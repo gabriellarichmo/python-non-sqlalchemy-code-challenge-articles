c7ddfb965e879e790045967d4e04687887e00674
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-03-20 15:21:52 -0700

    complete phase 3 code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,139 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        type(self).all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string.")
+        elif not 5 <= len(title) <= 50:
+            raise ValueError("Title must be between 5 and 50 chars inclusive.")
+        elif hasattr(self, "_title"):
+            raise AttributeError("Cannot change title after instantiation.")
+        else:
+            self._title = title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        if not isinstance(author, Author):
+            raise TypeError("Author must be of type Author.")
+        else:
+            self._author = author
+
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, magazine):
+        if not isinstance(magazine, Magazine):
+            raise TypeError("Magazine must be of type Magazine.")
+        else:
+            self._magazine = magazine
 class Author:
     def __init__(self, name):
-        self.name = name
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string.")
+
+    @property
+    def name(self):
+        return self._name
+
+    def __setattr__(self, name, value):
+        if hasattr(self, name):
+            raise AttributeError("Cannot modify attribute after instantiation.")
+        else:
+            super().__setattr__(name, value)
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author is self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self.articles():
+            return None
+        return list({article.magazine.category for article in self.articles()})
 
 class Magazine:
+    
     def __init__(self, name, category):
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and (2 <= len(name)) and (len(name) <= 16):
+            self._name = name
+        else:
+            raise ValueError("Name must be a string between 2 and 16 characters long.")
+
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, category):
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
+        else:
+            raise ValueError("Category must be a non-empty string.")
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine is self]
 
     def contributors(self):
-        pass
-
+        contributor_set = set()
+        for article in self.articles():
+            contributor_set.add(article.author)
+        return list(contributor_set)
+        # return list({ article.author for article in self.articles() })
+        
     def article_titles(self):
-        pass
+        if not self.articles():
+            return None
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        contributing_authors = []
+        for author in self.contributors():
+            article_count = sum(1 for article in author.articles() if article.magazine is self)
+            if article_count > 2:
+                contributing_authors.append(author)
+            if not contributing_authors:
+                return None
+            return contributing_authors
+
+    # @classmethod
+    # def top_publisher(cls, magazine):
+        # if magazine is None:
+        #     return None
+        
+        # if magazine_all_publications := [article for article in Article.all if article.magazine is magazine]:
+        #     return max(
+        #         cls.all,
+        #         key=lambda mag: sum(1 for article in magazine_all_publications if article.magazine is mag)
+        #     )
